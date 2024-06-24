619ce2988ec05c7d32945cc6520b12f0414b36fa
dskuzm <dskuzm@gmail.com>
2024-06-25 00:05:02 +0100

    rename file, update path

--- src/components/heroesList/heroesList.tsx
+++ src/components/heroesList/heroesList.tsx
@@ -20,7 +20,7 @@ const HeroesList: React.FC<HeroesListProps> = ({ heroes }) => {
     <>
       {heroes.map((item) => (
         <Grid key={item.id} item xs={12} sm={6} md={4} lg={3}>
-          <NextLink href={`/heroDetails/${item.name}`} passHref>
+          <NextLink href={item.name} passHref>
             <HeroCard hero={item} />
           </NextLink>
         </Grid>
